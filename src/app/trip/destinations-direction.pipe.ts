a72c98caa5d71e8d2f0e0e418178999cb77c16e6
Ralf Hauptmann <ralf.hauptmann84@gmail.com>
2018-05-08 22:32:20 +1000

    feat(trip): add DestinationsDirectionPipe transform functionality

--- src/app/trip/destinations-direction.pipe.ts
+++ src/app/trip/destinations-direction.pipe.ts
@@ -1,12 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { Destination } from '../shared/model/destination';
+
 @Pipe({
-  name: 'destinationsDirection'
+  name: 'destinationsDirection',
+  // pure: true // we could also set pure: false to make it impure but then angular will check this input with every circle
 })
 export class DestinationsDirectionPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(destinations: Destination[], args?: any): any[] {
+    return destinations.filter(function (destination: Destination, index: number, array: Destination[]): boolean {
+      if (destination.showDirection && index > 0) {
+        destination.directionsRequest = {
+          origin: { lat: array[index - 1].lat, lng: array[index - 1].lng },
+          destination: { lat: destination.lat, lng: destination.lng },
+          travelMode: google.maps.TravelMode.DRIVING
+        };
+        return true;
+      }
+      destination.directionsRequest = null;
+      return false;
+    });
   }
 
 }
